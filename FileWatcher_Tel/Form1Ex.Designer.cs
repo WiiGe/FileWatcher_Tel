e6002ac99f076fbd3939619ffec066afd4635585
WiiGe <wiige@live.com>
2014-09-30 09:15:50 +0800

    First Commit-No Change Log

---
+++ FileWatcher_Tel/Form1Ex.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace FileWatcher
+{
+	partial class Form1Ex
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.checkBoxIncludeSubfolders = new System.Windows.Forms.CheckBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonDone = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxFolderToWatch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new FileWatcher.ListViewNoFlicker();
+            this.columnTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnEvent = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnFilter = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnFileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonClearList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkBoxIncludeSubfolders
+            // 
+            this.checkBoxIncludeSubfolders.AutoSize = true;
+            this.checkBoxIncludeSubfolders.Location = new System.Drawing.Point(512, 10);
+            this.checkBoxIncludeSubfolders.Name = "checkBoxIncludeSubfolders";
+            this.checkBoxIncludeSubfolders.Size = new System.Drawing.Size(132, 16);
+            this.checkBoxIncludeSubfolders.TabIndex = 17;
+            this.checkBoxIncludeSubfolders.Text = "Include subfolders";
+            this.checkBoxIncludeSubfolders.UseVisualStyleBackColor = true;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBrowse.Location = new System.Drawing.Point(468, 6);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(33, 21);
+            this.buttonBrowse.TabIndex = 16;
+            this.buttonBrowse.Text = "...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 12);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Detected File System Events";
+            // 
+            // buttonDone
+            // 
+            this.buttonDone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonDone.Location = new System.Drawing.Point(361, 528);
+            this.buttonDone.Name = "buttonDone";
+            this.buttonDone.Size = new System.Drawing.Size(75, 21);
+            this.buttonDone.TabIndex = 14;
+            this.buttonDone.Text = "Done";
+            this.buttonDone.UseVisualStyleBackColor = true;
+            this.buttonDone.Click += new System.EventHandler(this.buttonDone_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonStop.Location = new System.Drawing.Point(276, 528);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 21);
+            this.buttonStop.TabIndex = 13;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonStart.Location = new System.Drawing.Point(191, 528);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 21);
+            this.buttonStart.TabIndex = 12;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBoxFolderToWatch
+            // 
+            this.textBoxFolderToWatch.Location = new System.Drawing.Point(90, 7);
+            this.textBoxFolderToWatch.Name = "textBoxFolderToWatch";
+            this.textBoxFolderToWatch.Size = new System.Drawing.Size(376, 21);
+            this.textBoxFolderToWatch.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Folder to watch";
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] 
+            {
+                this.columnTime,
+                this.columnEvent,
+                this.columnFilter,
+                this.columnFileName
+            });
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(7, 54);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(609, 468);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnTime
+            // 
+            this.columnTime.Text = "Time";
+            this.columnTime.Width = 50;
+            // 
+            // columnEvent
+            // 
+            this.columnEvent.Text = "Event";
+            this.columnEvent.Width = 80;
+            // 
+            // columnFilter
+            // 
+            this.columnFilter.Text = "Filter";
+            this.columnFilter.Width = 80;
+            // 
+            // columnFileName
+            // 
+            this.columnFileName.Text = "File Name";
+            this.columnFileName.Width = 395;
+            // 
+            // buttonClearList
+            // 
+            this.buttonClearList.Location = new System.Drawing.Point(151, 35);
+            this.buttonClearList.Name = "buttonClearList";
+            this.buttonClearList.Size = new System.Drawing.Size(75, 19);
+            this.buttonClearList.TabIndex = 18;
+            this.buttonClearList.Text = "Clear List";
+            this.buttonClearList.UseVisualStyleBackColor = true;
+            this.buttonClearList.Click += new System.EventHandler(this.buttonClearList_Click);
+            // 
+            // Form1Ex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 556);
+            this.Controls.Add(this.buttonClearList);
+            this.Controls.Add(this.checkBoxIncludeSubfolders);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonDone);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxFolderToWatch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1Ex";
+            this.Text = "File Watcher (Extended FileSystemWatcher)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1Ex_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.CheckBox checkBoxIncludeSubfolders;
+		private System.Windows.Forms.Button buttonBrowse;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button buttonDone;
+		private System.Windows.Forms.Button buttonStop;
+		private System.Windows.Forms.Button buttonStart;
+		private System.Windows.Forms.TextBox textBoxFolderToWatch;
+		private System.Windows.Forms.Label label1;
+		//private System.Windows.Forms.ListView listView1;
+		private ListViewNoFlicker listView1;
+		private System.Windows.Forms.ColumnHeader columnTime;
+		private System.Windows.Forms.ColumnHeader columnEvent;
+		private System.Windows.Forms.ColumnHeader columnFilter;
+		private System.Windows.Forms.ColumnHeader columnFileName;
+		private System.Windows.Forms.Button buttonClearList;
+	}
+}
